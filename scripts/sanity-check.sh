525910fd759eaacffd85dac6882943c67363c247
Louis Frayser <louis.frayser@gmail.com>
2019-12-05 09:25:01 -0800

    sanity check for startup

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -4,4 +4,4 @@ XMHOME=$HOME/.xmonad
 PIDFILE=$XMHOME/run/xmonad.pid
 [ -e $PIDFILE ] && read pid < $PIDFILE
 echo $PPID > $PIDFILE
-[ "$pid" = $PPID ] || exec $XMHOME/scripts/spawnOnce
+[ "$pid" = $PPID ] || exec $XMHOME/scripts/spawnOnce.sh
--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 #xrandr.cmd
-pulseaudion -D
+pulseaudio  -D
 xsetroot -bg '#773e3e' -fg '#c0c0b0'  -mod 16 16
 xdaliclock -24 -cycle -bw 4 -transparent &
 terminator&
