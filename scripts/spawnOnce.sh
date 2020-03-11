80988daff3bbd6ecf275b48e668c61adbb34f855
Louis Frayser <louis.frayser@gmail.com>
2020-03-11 09:26:59 -0700

    Changed some startup clients

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 #xrandr.cmd
-pulseaudio  -D
-xsetroot -bg '#773e3e' -fg '#c0c0b0'  -mod 16 16
-xdaliclock -24 -cycle -bw 4 -transparent &
-terminator&
+#pulseaudio  -D
+#xsetroot -bg '#773e3e' -fg '#c0c0b0'  -mod 16 16
+fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
+
+#xdaliclock -24 -cycle -bw 4 -transparent &
+terminator --profile pretty &
 xscreensaver -no-splash&
 
