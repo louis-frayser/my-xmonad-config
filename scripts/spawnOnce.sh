f3a135e117cbe88580006b00b00edd9d76b07c64
Louis Frayser <louis.frayser@gmail.com>
2020-09-03 00:57:14 -0700

    cosmetic

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -2,10 +2,15 @@
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 #xrandr.cmd
 #pulseaudio  -D
-#xsetroot -bg '#773e3e' -fg '#c0c0b0'  -mod 16 16
-fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
+xsetroot -bg '#773e3e' -fg '#c0c0b0'  -mod 16 16
+
+## Background
+#fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
+fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"
 
 #xdaliclock -24 -cycle -bw 4 -transparent &
 { terminator --profile pretty || konsole || mate-terminal;} &
 xscreensaver -no-splash&
+chrome &
+
 
