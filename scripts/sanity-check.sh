c8e5723ceb90aaad3b14febdbfed19509011adf5
Louis Frayser <louis.frayser@gmail.com>
2020-09-03 00:58:09 -0700

    More rebust mangement of PID file

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
 XMHOME=$HOME/.xmonad
-PIDFILE=$XMHOME/run/xmonad.pid
-[ -e $PIDFILE ] && read pid < $PIDFILE
+RUNDIR=$XMHOME/run
+PIDFILE=$RUNDIR/monad.pid
+if [ -e $PIDFILE ] 
+then read pid < $PIDFILE
+else
+  [ -d $RUNDIR ] || mkdir $RUNDIR
+fi
 echo $PPID > $PIDFILE
 xhost local:
 [ "$pid" = $PPID ] || exec $XMHOME/scripts/spawnOnce.sh
