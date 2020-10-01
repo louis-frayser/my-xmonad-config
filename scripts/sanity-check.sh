23f601b12146af2405f69b6d074c7ade2781abb7
Louis Frayser <louis.frayser@gmail.com>
2020-10-01 03:51:02 -0700

    Fixed xmonad PID detection for conditional restarts Added detection of missing auxillary programs xmobar, grun, dmenu.

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -2,12 +2,52 @@
 
 XMHOME=$HOME/.xmonad
 RUNDIR=$XMHOME/run
-PIDFILE=$RUNDIR/monad.pid
+PIDFILE=$RUNDIR/xmonad.pid
+
+ppids () 
+{ 
+    local pid=${1:-$$};
+    echo $pid;
+    if [ $pid -ne 1 ]; then
+        ppids $( ps -h -oppid $pid);
+    fi
+}
+
+## --------------------------------------------------
+xmpid(){
+    ps -h -opid,fname $(ppids) |
+	while read pid fname
+	do case $fname in xmonad*) echo $pid
+				   break
+				   ;;
+	   esac
+	done
+}
+
+
 if [ -e $PIDFILE ] 
 then read pid < $PIDFILE
 else
   [ -d $RUNDIR ] || mkdir $RUNDIR
 fi
-echo $PPID > $PIDFILE
+XMPID=$(xmpid)
+echo $XMPID > $PIDFILE
+
 xhost local:
-[ "$pid" = $PPID ] || exec $XMHOME/scripts/spawnOnce.sh
+xmessage XMPID: $XMPID
+
+### Run if not already run
+[ "$pid" = $XMPID ] || exec $XMHOME/scripts/spawnOnce.sh
+
+
+### Verify the necesary auxilary packages are installed.
+
+echo "Verifying auxillary packages..."
+for x in xmessage xmobar grun dmenu
+do echo -n "$x: "
+   if which $x
+   then echo "OK."
+   else echo "Missing!"
+	xmessage "Package $x is missing"
+   fi
+done
