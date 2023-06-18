ecbad65b16217483338609564d24773b10056659
Louis Frayser <louis.frayser@gmail.com>
2023-06-18 13:19:01 -0700

    Adden settings daemon to get the proper colors on app windows

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -9,7 +9,7 @@ xsetroot -bg 'gray30' -fg '#c0c0b0'  -mod 16 16
 ## Background
 #fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
 fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"
-
+/usr/libexec/mate-settings-daemon&
 #xdaliclock -24 -cycle -bw 4 -transparent &
 # { terminator --profile pretty || konsole || mate-terminal;} &
 xscreensaver -no-splash&
