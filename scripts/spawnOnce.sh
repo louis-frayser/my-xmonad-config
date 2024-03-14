80b189d4065bdd6ee5b40f95a9b7cbf1d8bb8029
Louis Frayser <louis.frayser@gmail.com>
2024-03-14 03:46:19 -0700

    Fixed problem with keyring environment not configured properly

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 # NOTE:  The scrpt needs to return -- run long-running process in the background  
+set -x
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 xrdb -merge ~/.Xresources
+setxkbmap -option "compose:lwin"
 #xrandr.cmd
-#pulseaudio  -D
+pulseaudio  -D
 xsetroot -bg 'gray30' -fg '#c0c0b0'  -mod 16 16
 
 ## Background
 #fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
 fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"
-/usr/libexec/mate-settings-daemon&
-#xdaliclock -24 -cycle -bw 4 -transparent &
-# { terminator --profile pretty || konsole || mate-terminal;} &
-xscreensaver -no-splash&
 
 ## Warn user to mount the encrypted ~/doc directory
 mountpoint ~/doc || { xmessage -font "-*-lucida-*-r-*-*-24-*-*-*-*-*-*-*" "Mount the crypt! Press [Okay]!" & }
@@ -23,7 +21,8 @@ mountpoint ~/doc || { xmessage -font "-*-lucida-*-r-*-*-24-*-*-*-*-*-*-*" "Mount
 source /etc/X11/xinit/xinitrc.d/?0-dbus
 
 # Start or connect Gnome-Keyring
-eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+eval $(/usr/bin/gnome-keyring-daemon --replace -f --components=gpg,pkcs11,secrets,ssh) &
+: ${GNOME_KEYRING_PID:=$!}
 
 # You probably need to do this too:
 export SSH_AUTH_SOCK
@@ -31,6 +30,14 @@ export GPG_AGENT_INFO
 export GNOME_KEYRING_CONTROL
 export GNOME_KEYRING_PID
 }
+
+/usr/libexec/mate-settings-daemon&
+#xdaliclock -24 -cycle -bw 4 -transparent &
+# { terminator --profile pretty || konsole || mate-terminal;} &
+xscreensaver -no-splash&
+
+## Browser
 xdg-open "http://localhost:8008" &
+
 exit 0
 
