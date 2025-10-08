9cae5432c39a83fcde8f273193768f916094ee85
Louis Frayser <louis.frayser@gmail.com>
2025-10-08 10:44:49 -0700

    Scripted mounted of the_crypt

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -1,19 +1,24 @@
-#! /bin/bash
+e! /bin/bash
 # NOTE:  The scrpt needs to return -- run long-running process in the background  
 set -x
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 xrdb -merge ~/.Xresources
 setxkbmap -option "compose:lwin"
-#xrandr.cmd
 pulseaudio  -D
 xsetroot -bg 'gray30' -fg '#c0c0b0'  -mod 16 16
 
 ## Background
 #fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
 fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"
-
-## Warn user to mount the encrypted ~/doc directory
-mountpoint ~/doc || { xmessage -font "-*-lucida-*-r-*-*-24-*-*-*-*-*-*-*" "Mount the crypt! Press [Okay]!" & }
+for x in {1..2};do sudo terminator& sleep 1;done
+# Mount encrypted directories; and launh a root term while we still have su.
+if ! mountpoint ${HOME}/doc
+then xterm -e  \
+   sh -c  'echo "Mounting the crypt...";\
+           sudo ~toor/bin/mount-the-crypt ||  \
+              read -p "problems!:"; \
+           read -p ":";terminator&'
+fi
 
 {
 # https://wiki.parabola.nu/GNOME_Keyring#Use_Without_Gnome 2021-05-20
@@ -34,9 +39,13 @@ export GNOME_KEYRING_PID
 #xdaliclock -24 -cycle -bw 4 -transparent &
 # { terminator --profile pretty || konsole || mate-terminal;} &
 xscreensaver -no-splash&
+redshift &  # Adjust screen colortemp for night/day
 
 ## Browser
-xdg-open "http://localhost:8008" &
+## Disabled to prevent writing the cache & profile
+##directories before I can set ramdisks for them
+## Chrome writes too often for disk's health
+##xdg-open "http://localhost:8008" &
 
 exit 0
 
