241b36a78f57244e19877ac7deae7b1194445b81
Louis Frayser <louis.frayser@gmail.com>
2026-01-24 18:33:29 -0800

    Added launch of terminals and attempts to better control desktp background upon startup

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -1,16 +1,17 @@
-e! /bin/bash
+#! /bin/bash
 # NOTE:  The scrpt needs to return -- run long-running process in the background  
 set -x
 export PATH=~/bin:/usr/lucho/bin:/usr/local/bin:/bin:$PATH
 xrdb -merge ~/.Xresources
 setxkbmap -option "compose:lwin"
-pulseaudio  -D
 xsetroot -bg 'gray30' -fg '#c0c0b0'  -mod 16 16
 
 ## Background
 #fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
-fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"
-for x in {1..2};do sudo terminator& sleep 1;done
+
+## Spawn two root terminals
+for x in {1..2};do sudo -u toor terminator& sleep 1;done
+
 # Mount encrypted directories; and launh a root term while we still have su.
 if ! mountpoint ${HOME}/doc
 then xterm -e  \
@@ -23,7 +24,11 @@ fi
 {
 # https://wiki.parabola.nu/GNOME_Keyring#Use_Without_Gnome 2021-05-20
 # Start a dbus-session if not already running
+
 source /etc/X11/xinit/xinitrc.d/?0-dbus
+
+pulseaudio  -D
+#gentoo-pipewire-launcher &
 
 # Start or connect Gnome-Keyring
 eval $(/usr/bin/gnome-keyring-daemon --start --daemonize --components=gpg,pkcs11,secrets,ssh)
@@ -39,13 +44,18 @@ export GNOME_KEYRING_PID
 #xdaliclock -24 -cycle -bw 4 -transparent &
 # { terminator --profile pretty || konsole || mate-terminal;} &
 xscreensaver -no-splash&
-redshift &  # Adjust screen colortemp for night/day
+pidof redshift || redshift &  # Adjust screen colortemp for night/day
 
 ## Browser
 ## Disabled to prevent writing the cache & profile
 ##directories before I can set ramdisks for them
 ## Chrome writes too often for disk's health
-##xdg-open "http://localhost:8008" &
-
+if mountpoint ${HOME}/.config/google-chrome
+then #xdg-open "http://localhost:8008" &
+     : xdg-open "http://localhost:8008" &
+fi
+## Gettign a weird background. Trying to overide
+{ sleep 16; fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"; } &
+{ sleep 1m; fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"; } &
 exit 0
 
