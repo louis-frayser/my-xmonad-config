db6bcaf0070462a1dfb0f7b28058d6c01fc3e77c
Louis Frayser <louis.frayser@gmail.com>
2026-03-12 20:54:10 -0700

    Added more detailed sequencing or start-up processes

--- scripts/spawnOnce.sh
+++ scripts/spawnOnce.sh
@@ -12,10 +12,16 @@ xsetroot -bg 'gray30' -fg '#c0c0b0'  -mod 16 16
 #fbsetbg -t /usr/share/backgrounds/larry-the-cow/gentoo-larry-bg-1920x1080.png
 
 ## Spawn two root terminals
-for x in {1..2};do sudo -u toor terminator& sleep 1;done
+xmonadctl -a goto 1
+xmonadctl -a goto left
+sudo -u toor env -u XDG_RUNTIME_DIR terminator&
+sleep  1
+xmonadctl -a goto right
+sudo -u toor env -u XDG_RUNTIME_DIR terminator&
+sleep 1
 
 # Mount encrypted directories; and launh a root term while we still have su.
-if ! mountpoint ${HOME}/doc
+if ! mountpoint "${HOME}/doc"
 then xterm -e  \
    sh -c  'echo "Mounting the crypt...";\
            sudo ~toor/bin/mount-the-crypt ||  \
@@ -27,13 +33,13 @@ fi
 # https://wiki.parabola.nu/GNOME_Keyring#Use_Without_Gnome 2021-05-20
 # Start a dbus-session if not already running
 
-source /etc/X11/xinit/xinitrc.d/?0-dbus
+source /etc/X11/xinit/xinitrc.d/80-dbus
 
 pulseaudio  -D
 #gentoo-pipewire-launcher &
 
 # Start or connect Gnome-Keyring
-eval $(/usr/bin/gnome-keyring-daemon --start --daemonize --components=gpg,pkcs11,secrets,ssh)
+eval "$(/usr/bin/gnome-keyring-daemon --start --daemonize --components=gpg,pkcs11,secrets,ssh)"
 
 # You probably need to do this too:
 export SSH_AUTH_SOCK
@@ -52,11 +58,27 @@ pidof redshift || redshift &  # Adjust screen colortemp for night/day
 ## Disabled to prevent writing the cache & profile
 ##directories before I can set ramdisks for them
 ## Chrome writes too often for disk's health
-if mountpoint ${HOME}/.config/google-chrome
-then #xdg-open "http://localhost:8008" &
-     : xdg-open "http://localhost:8008" &
-fi
+launch_chrome(){
+ local c1 c2
+  cntcro(){ ps -C chrome|wc -l; }
+
+  if mountpoint "${HOME}/.config/google-chrome"
+  then #xdg-open "http://localhost:8008" &
+        xmonadctl -a goto 2
+        xdg-open "http://localhost:8008" &
+  fi
+
+  for ((c1=0, c2=99; c1 != c2; ))
+  do sleep 1
+     c1=$(cntcro); c2=$(cntcro)
+  done
+}
+
+xmonadctl -a goto 10
+drracket &
+
 ## Gettign a weird background. Trying to overide
+fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg" &
 { sleep 16; fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"; } &
 { sleep 1m; fbsetbg -t "/export/images/public/Wallpaper/Hot Chix/tatoo-lady.jpg"; } &
 exit 0
